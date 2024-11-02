a3840eedaa07bc1021a8297be4de28797ff8a486
AdarhKumar <adarshnit420@gmail.com>
2024-11-02 07:29:01 +0530

    first commit

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 // Task routes
 router.post('/create', authMiddleware, createTask);
-router.get('/', getAllTasks);
+router.get('/', authMiddleware, getAllTasks);
 router.put('/:taskId', authMiddleware, updateTask);
 router.delete('/:taskId', authMiddleware, deleteTask);
 router.get('/filter', authMiddleware, filterTasks);
